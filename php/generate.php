d920ac7d7cad30a2d71ea2c7081bb34691e7c9b8
poell <S143806@student.hb.se>
2017-09-04 23:38:17 +0200

    error 405?

--- php/generate.php
+++ php/generate.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-Huvudtitel 245/a  <?php echo $_GET["titel"]; ?> ;<br>
-Upphovsuppgift/245c <?php echo $_GET["forfattare"]; ?> ;
+Huvudtitel 245/a  <?php echo $_POST["titel"]; ?> ;<br>
+Upphovsuppgift/245c <?php echo $_POST["forfattare"]; ?> ;
 
 </body>
 </html>
