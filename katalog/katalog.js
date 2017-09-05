d24da9da93ecfd6e2c1733af83aa78bfc0b5e8a3
poell <S143806@student.hb.se>
2017-09-05 14:42:58 +0200

    katalogupd

---
+++ katalog/katalog.js
@@ -0,0 +1,24 @@
+
+$('#manifestation').keyup(function () {
+  $('#displayManifestation').text($(this).val());
+});
+$('#isbn').keyup(function () {
+  $('#displayIsbn').text($(this).val());
+});
+$('#issn').keyup(function () {
+  $('#displayIssn').text($(this).val());
+});
+$('#titel').keyup(function () {
+  $('#displayTitel').text($(this).val());
+});
+$('#undertitel').keyup(function () {
+  $('#displayUndertitel').text($(this).val());
+});
+
+$('#upphov').keyup(function () {
+  $('#displayUpphov').text($(this).val());
+});
+$('#upplaga').keyup(function () {
+  $('#displayUpplaga').text($(this).val());
+});
+
