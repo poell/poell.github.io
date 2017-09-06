32dc5674cceb58a8e0b76fdf097f457f1474fa22
poell <S143806@student.hb.se>
2017-09-06 19:29:43 +0200

    stressed out

--- searchapp.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('stockFinder', [])
-app.controller('liststock', function($scope, $http)  {
-	$scope.stocked = [] 
-	$http.get("https://api.myjson.com/bins/1o28q.json").success(function(response) {
-		$scope.stocked = response;  
-        });
-});
-
