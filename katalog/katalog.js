510108e72c724ed83b21faaab7b796ce12c84c93
poell <S143806@student.hb.se>
2017-09-06 10:42:14 +0200

    bilder

--- katalog/katalog.js
+++ katalog/katalog.js
@@ -47,12 +47,79 @@ $('#bararomfang').keyup(function () {
 $('#huvudtitelserie').keyup(function () {
   $('#displayHuvudtitelserie').text($(this).val());
 });
+$('#seriemanifestation').keyup(function () {
+  $('#displaySeriemanifestation').text($(this).val());
+});
+$('#seriemanifestation').keyup(function () {
+  $('#displaySeriemanifestation2').text($(this).val());
+});
+$('#verkmanifestation').keyup(function () {
+  $('#displayVerkmanifestation').text($(this).val());
+});
+$('#verkmanifestation').keyup(function () {
+  $('#displayVerkmanifestation2').text($(this).val());
+});
+$('#relhuvudtitel1').keyup(function () {
+  $('#displayRelhuvudtitel1').text($(this).val());
+});
+$('#relhuvudtitel2').keyup(function () {
+  $('#displayRelhuvudtitel2').text($(this).val());
+});
+$('#person1').keyup(function () {
+  $('#displayPerson1').text($(this).val());
+});
+$('#fornamn1').keyup(function () {
+  $('#displayFornamn1').text($(this).val());
+});
+$('#levnad1').keyup(function () {
+  $('#displayLevnad1').text($(this).val());
+});
+$('#viaf1').keyup(function () {
+  $('#displayViaf1').text($(this).val());
+});
+$('#person2').keyup(function () {
+  $('#displayPerson2').text($(this).val());
+});
+$('#fornamn2').keyup(function () {
+  $('#displayFornamn2').text($(this).val());
+});
+$('#levnad2').keyup(function () {
+  $('#displayLevnad2').text($(this).val());
+});
+$('#viaf2').keyup(function () {
+  $('#displayViaf2').text($(this).val());
+});
+$('#person3').keyup(function () {
+  $('#displayPerson3').text($(this).val());
+});
+$('#fornamn3').keyup(function () {
+  $('#displayFornamn3').text($(this).val());
+});
+$('#levnad3').keyup(function () {
+  $('#displayLevnad3').text($(this).val());
+});
+$('#viaf3').keyup(function () {
+  $('#displayViaf3').text($(this).val());
+});
+$('#huvudupphov').keyup(function () {
+  $('#displayHuvudupphov').text($(this).val());
+});
 $('#medupphov1').keyup(function () {
   $('#displayMedupphov1').text($(this).val());
+});
+$('#medupphov2').keyup(function () {
+  $('#displayMedupphov2').text($(this).val());
 });
 $('#roll1').keyup(function () {
   $('#displayRoll1').text($(this).val());
 });
+$('#roll2').keyup(function () {
+  $('#displayRoll2').text($(this).val());
+});
+$('#roll3').keyup(function () {
+  $('#displayRoll3').text($(this).val());
+});
+
 $( function() {
     $( document ).tooltip();
   });
